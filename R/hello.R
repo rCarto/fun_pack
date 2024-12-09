c00fc871ed545912f1dc1a05f0ef0f8a2b5c2fed
rCarto <timothee.giraud@cnrs.fr>
2024-12-09 12:19:06 +0100

    initial commit

---
+++ R/hello.R
@@ -0,0 +1,44 @@
+#' Nom de la fonction
+#'
+#' Description de la fonction
+#' @param x definition param x
+#' @param z definition param z
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' un exemple de fonction
+#'
+#'
+#' }
+fonction1 <- function(x) {
+  print("Hello, world!")
+}
+
+
+#' Nom de la fonction 2
+#'
+#' Description de la fonction une autre donction
+#' @param x definition param x
+#' @param z definition param x
+#' @return
+#' @export
+#' @details
+#' Additional details... ce que tu veux
+#'
+#' @note
+#' dmjzdfjsgjsg
+#'
+#' sdhshqd
+#'
+#'
+#'
+#' @seealso  titit totot
+#'
+#'
+#' @examples
+fonctionx <- function(x) {
+  print("Hello, world!")
+}
